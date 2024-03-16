1aff822c470e28d8a6852b62e438de4a89c7dd37
55j <1056637702@qq.com>
2024-03-16 16:48:51 +0800

    first commit

---
+++ src/main/java/com/clothingd/xiaopu/XiaopuApplication.java
@@ -0,0 +1,13 @@
+package com.clothingd.xiaopu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaopuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaopuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clothingd/xiaopu/XiaopuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clothingd.xiaopu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaopuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
